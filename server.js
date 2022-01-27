7de02c9911020f1721f92371b99c6c97ec32c491
Ty D'Angelo <tydangelo18@gmail.com>
2022-01-27 16:25:46 -0600

    Set up basic GraphQL and Apollo Server and test run on port.

---
+++ server.js
@@ -0,0 +1,24 @@
+const { ApolloServer } = require("apollo-server");
+const gql = require("graphql-tag");
+
+const typeDefs = gql`
+  type Query {
+    sayHi: String!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    sayHi: () => "Testing... Hello!",
+  },
+};
+
+// Apollo Server Instance
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+server.listen({ port: 5000 }).then((res) => {
+  console.log(`Server running at ${res.url}`);
+});
