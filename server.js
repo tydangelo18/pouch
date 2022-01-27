f8a2828e26adaf5105b71f83f80391882a137254
Ty D'Angelo <tydangelo18@gmail.com>
2022-01-27 17:46:35 -0600

    Connected server to MongoDB Database.
    Signed-off-by: Ty D'Angelo <tydangelo18@gmail.com>

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const { ApolloServer } = require("apollo-server");
 const gql = require("graphql-tag");
+const mongoose = require("mongoose");
+
+const { MONGODB } = require("./config/config");
 
 const typeDefs = gql`
   type Query {
@@ -19,6 +22,12 @@ const server = new ApolloServer({
   resolvers,
 });
 
-server.listen({ port: 5000 }).then((res) => {
-  console.log(`Server running at ${res.url}`);
-});
+mongoose
+  .connect(MONGODB, { useNewUrlParser: true })
+  .then(() => {
+    console.log("Connected to DB");
+    return server.listen({ port: 5000 });
+  })
+  .then((res) => {
+    console.log(`Server running at ${res.url}`);
+  });
