26e10cc799ecc6a3751057bc7a7f07dbbcd7cd75
Ty D'Angelo <tydangelo18@gmail.com>
2022-01-30 17:30:42 -0600

    Create conditional to find email used in registration of user and if found in database, throw error.
    Signed-off-by: Ty D'Angelo <tydangelo18@gmail.com>

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -1,14 +1,30 @@
 const User = require("../../models/User");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+// Dependency For Apollo to recognize errors and display it
+const { UserInputError } = require("apollo-server");
 const { SECRET_KEY } = require("../../config/config");
 
 module.exports = {
   Mutation: {
     async register(_, { registerInput: { email, password } }) {
       // TODO: Validate the user data for logging in
-      // TODO: Make sure user doesn't already exist so no duplicate users can be stored into the database (same email)
 
+      // Make sure user doesn't already exist so no duplicate users can be stored into the database (same email)
+      const user = await User.findOne({ email });
+      // if user already exists => return an error
+      if (user) {
+        throw new UserInputError(
+          // error will be dislayed on front end if condition is met
+          "This email is already being used by another account, please use another email",
+          {
+            errors: {
+              email:
+                "This email is already being used by another account, please use another email",
+            },
+          }
+        );
+      }
       // Hash password before storing to DB
       password = await bcrypt.hash(password, 12);
 
