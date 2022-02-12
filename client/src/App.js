8a5eb8e04644ed6f2bae28c0a4ff60b480166a32
Ty D'Angelo <tydangelo18@gmail.com>
2022-02-12 13:26:43 -0600

    Created Routes in Parent Component for Register, Login and Home Page.
    Signed-off-by: Ty D'Angelo <tydangelo18@gmail.com>

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,21 @@
+// React Router
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import "./App.css";
+import Home from "./pages/Home";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
 
 function App() {
   return (
     <div>
-      <h1>Testing 123...</h1>
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/login" element={<Login />} />
+          <Route exact path="/register" element={<Register />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
